dfd84b52bedf65bdcdc8ab9a80712cf7d006a151
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2025-07-21 22:34:19 +0530

    `Update download filename in editor.tsx from 'viewport-image.png' to 'thought.png'`

--- ui/editor.tsx
+++ ui/editor.tsx
@@ -104,7 +104,7 @@ export default function Editor() {
       }).then((canvas) => {
         const link = document.createElement('a');
         link.href = canvas.toDataURL('image/png');
-        link.download = 'viewport-image.png';
+        link.download = 'thought.png';
         link.click();
       });
     }
